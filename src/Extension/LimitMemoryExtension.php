4250916f5fd7e1768e008e5846ab638be164b908
Hannes Schulz <schulz@daten-bahn.de>
2019-11-16 22:19:07 +0100

    add limit extensions to preProvideSchedule

--- src/Extension/LimitMemoryExtension.php
+++ src/Extension/LimitMemoryExtension.php
@@ -3,12 +3,14 @@
 namespace Abc\Scheduler\Extension;
 
 use Abc\Scheduler\Context\PreProcessSchedule;
+use Abc\Scheduler\Context\PreProvideSchedules;
 use Abc\Scheduler\Context\ScheduleProcessed;
 use Abc\Scheduler\PreProcessScheduleExtensionInterface;
+use Abc\Scheduler\PreProvideSchedulesExtensionInterface;
 use Abc\Scheduler\ScheduleProcessedExtensionInterface;
 use Psr\Log\LoggerInterface;
 
-class LimitMemoryExtension implements PreProcessScheduleExtensionInterface, ScheduleProcessedExtensionInterface
+class LimitMemoryExtension implements PreProcessScheduleExtensionInterface, ScheduleProcessedExtensionInterface, PreProvideSchedulesExtensionInterface
 {
     /**
      * @var int
@@ -21,13 +23,17 @@ class LimitMemoryExtension implements PreProcessScheduleExtensionInterface, Sche
     public function __construct($memoryLimit)
     {
         if (false == is_int($memoryLimit)) {
-            throw new \InvalidArgumentException(sprintf(
-                'Expected memory limit is int but got: "%s"',
-                is_object($memoryLimit) ? get_class($memoryLimit) : gettype($memoryLimit)
-            ));
+            throw new \InvalidArgumentException(sprintf('Expected memory limit is int but got: "%s"', is_object($memoryLimit) ? get_class($memoryLimit) : gettype($memoryLimit)));
         }
 
         $this->memoryLimit = $memoryLimit * 1024 * 1024;
+    }
+
+    public function onPreProvideSchedules(PreProvideSchedules $context): void
+    {
+        if ($this->shouldBeStopped($context->getLogger())) {
+            $context->interruptExecution();
+        }
     }
 
     public function onPreProcessSchedule(PreProcessSchedule $context): void
@@ -48,11 +54,7 @@ class LimitMemoryExtension implements PreProcessScheduleExtensionInterface, Sche
     {
         $memoryUsage = memory_get_usage(true);
         if ($memoryUsage >= $this->memoryLimit) {
-            $logger->debug(sprintf(
-                '[LimitMemoryExtension] Interrupt execution as memory limit reached. limit: "%s", used: "%s"',
-                $this->memoryLimit,
-                $memoryUsage
-            ));
+            $logger->debug(sprintf('[LimitMemoryExtension] Interrupt execution as memory limit reached. limit: "%s", used: "%s"', $this->memoryLimit, $memoryUsage));
 
             return true;
         }
